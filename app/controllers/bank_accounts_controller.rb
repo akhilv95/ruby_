2d26348005eea70bb4ccc79ebb2f14e8b3a6a383
Akhil Valanky <akhilvcarrer@gmail.com>
2025-02-11 00:22:53 +0530

    wrote the logic in acount_controller

--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -4,6 +4,43 @@ class BankAccountsController < ApplicationController
   end
 
   def invest
-    
+    investment_amount=params[:investment_amount].to_i
+    accounts=BankAccount.all.map do |account|
+      available_balance = account.enforce_min_balance ? (account.balance - account.min_balance) : account.balance
+      { name:account.name, balance:account.balance, available:available_balance }
   end
+  allocation = allocate_funds(accounts,investment_amount)
+  if allocation.empty?
+    @result= "NO MATCH"
+  else
+    @result= allocation
+  end
+  @bank_accounts = BankAccount.all
+  render :index
+
+end
+private
+
+def allocate_funds(accounts, amount)
+  accounts.sort_by! { |acc| acc[:available]}
+
+  match= accounts.find{ |acc| acc[:available] == amount}
+  return [{name: match[:name], amount:amount}] if match
+  greater_match = accounts.find { |acc| acc[:available] > amount }
+  return [{ name: greater_match[:name], amount: amount }] if greater_match
+
+  selected_accounts = []
+  total = 0
+
+  accounts.reverse_each do |acc|
+    next if acc[:available] <= 0
+    taken = [acc[:available], amount - total].min
+    selected_accounts << { name: acc[:name], amount: taken }
+    total += taken
+    break if total >= amount
+  end
+
+  return selected_accounts if total == amount
+  return [] 
+end
 end
